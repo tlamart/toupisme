86e6ad218d7b2a5214bcdc5892b5a863057a4ecd
thibal <thibal@42network.org>
2021-08-03 00:55:30 +0200

    add quote

--- createLink.js
+++ createLink.js
@@ -1,6 +1,6 @@
 const queryString = window.location.search;
 const UrlParams = new URLSearchParams(queryString);
 
-let linkText = "https://tlamart.github.io/toupisme/toupisme.html?ans=" + UrlParams.get("ans") + "&req=" + UrlParams.get("req")
+let linkText = "https://tlamart.github.io/toupisme/toupisme.html?ans='" + UrlParams.get("ans") + "'&req='" + UrlParams.get("req") + "'"
 let link = document.getElementById('link')
 link.innerText = "Here is your link : " + linkText
