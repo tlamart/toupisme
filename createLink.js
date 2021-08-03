a5c0f06061bc7cafa72e92e0123cde6bef3a876f
thibal <thibal@42network.org>
2021-08-03 10:48:40 +0200

    linkify

--- createLink.js
+++ createLink.js
@@ -1,6 +1,11 @@
 const queryString = window.location.search;
 const UrlParams = new URLSearchParams(queryString);
 
-let linkText = "https://tlamart.github.io/toupisme/toupisme.html?ans='" + UrlParams.get("ans") + "'&req='" + UrlParams.get("req") + "'"
+let ans = UrlParams.get("ans")
+let req = UrlParams.get("req")
+req = req.split(' ').join('%20')
+
+let linkText = "https://tlamart.github.io/toupisme/toupisme.html?ans='" + ans + "'&req='" + req + "'"
 let link = document.getElementById('link')
-link.innerText = "Here is your link : " + linkText
+// link.innerText = "Here is your link : " + linkText
+link.setAttribute('href', linkText)
