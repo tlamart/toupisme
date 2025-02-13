2736acb972ab12e437eb75785bae677c99537d98
tlamart <46746254+tlamart@users.noreply.github.com>
2025-02-13 14:50:02 +0100

    Update createLink.js

--- createLink.js
+++ createLink.js
@@ -6,7 +6,7 @@ let req = UrlParams.get("req")
 req = req.split(' ').join('%20')
 
 // let linkText = "https://toupisme.netlify.app/toupisme.html?ans='" + ans + "'&req='" + req + "'"
-let linkText = "/toupisme.html?ans='" + ans + "'&req='" + req + "'"
+let linkText = "./toupisme.html?ans='" + ans + "'&req='" + req + "'"
 let link = document.getElementById('link')
 // link.innerText = "Here is your link : " + linkText
 link.setAttribute('href', linkText)
